c5dac94f1f506dfc252b3e85dd2ef0db74fa279a
JulienUsson <usson.julien@gmail.com>
2015-12-12 11:48:06 +0100

    Ajout de AngularJS

---
+++ web/application/views/angular.php
@@ -0,0 +1,17 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="fr">
+	<head>
+		<meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Dwivit</title>
+		<script src="/assets/lib/angular.min.js"></script>
+		<script src="/assets/lib/angular-route.min.js"></script>
+		<script src="/assets/js/app.js"></script>
+	</head>
+	<body>
+		<div ng-view></div>
+	</body>
+</html>
---
+++ web/assets/js/app.js
@@ -0,0 +1,11 @@
+var app = angular.module('Dwivit', ['ngRoute']);
+
+app.config(function($routeProvider) {
+	$routeProvider.
+    when('/', {
+			templateUrl : '/assets/templates/home.html'
+    }).
+    otherwise({
+			redirectTo: '/'
+		});
+});
